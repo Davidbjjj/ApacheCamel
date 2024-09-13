2eee36e3d413a04ad536b7dd7fe2c6c3f227f53a
David Pontes <60227838+Davidbjjj@users.noreply.github.com>
2024-09-13 18:59:39 +0000

    add:camel logs

--- src/main/java/com/example/demo/route/FileRoute.java
+++ src/main/java/com/example/demo/route/FileRoute.java
@@ -6,10 +6,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class FileRoute extends RouteBuilder {
     private String pastaE="C:\\tmp\\entrada\\";
+    // usamos o '?delete=true' para apagar a pasta .camel
     @Override
     public void configure() throws Exception {
-        from("file://"+pastaE+"input")
+        //na pasta, vai criar uma parta 'input' onde todos os arquivos daqui, vao parao to
+        from("file://"+pastaE+"input?delete=true")
+                //log()usamos para saber informações
+                //log("${file:name}") para pegar o nome do arquivo.... file.getName está para compositor
+                .log("${file:name}")
+                //log("Arquivo:${header.CamelFileName} - Path: ${header.CamelFilePath} fala os endereços do arquivo e a pasta onde o arquivo está enserido
+                //aqui ele vai pear o nome do arquvo
+                .log("Arquivo:${header.CamelFileName}")
+                //aqui o log vai mostar o endereço da pasta
+                .log("Pasta:${header.CamelFilePath}")
+                //ele pegar os arquivos de input e manda para o output
                 .to("file://"+pastaE+"output");
-
     }
 }
